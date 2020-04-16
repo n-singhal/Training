6a3a4de7dee2c599c3ce887becd38d0cc91b5797
n-singhal <63765508+n-singhal@users.noreply.github.com>
2020-04-16 08:51:27 +0530

    Add files via upload

---
+++ GoLangTraining/Day1/Number_Pyramid.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+        x := 1
+	for i := 0; i < 5; i++{
+		for j := 0; j <= i; j++{
+			fmt.Printf("%d ", x)
+			x++
+		}
+		fmt.Println()
+	}
+}
